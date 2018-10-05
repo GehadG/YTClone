954fc5da2929aa9ab1c85f87ec52aeb610a59084
Gehad <gehad4195@gmail.com>
2018-10-05 04:27:23 +0200

    Init app

---
+++ app/src/main/java/com/clone/youtube/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clone.youtube.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.clone.youtube.youtubeclone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
